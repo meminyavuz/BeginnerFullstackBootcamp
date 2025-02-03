1e6c5c42b067722e07ac6e17f8235fb07ee74ea2
Mehmet Emin Yavuz <yavuz.m.e@outlook.com>
2025-02-03 22:06:26 +0300

    Added EF Core NuGet Packages

--- LibraryManagementSystem/obj/Debug/net9.0/LibraryManagementSystem.AssemblyInfo.cs
+++ LibraryManagementSystem/obj/Debug/net9.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa3336dbd42e19d4543d059fd3a46830dbbf44a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed011840457116267c73dc37a1e0a87060df3cb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
